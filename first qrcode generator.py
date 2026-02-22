d5b7b6fd81e5a4fa8b8d00a32cdeea84927d850a
sarthak1527 <153484219+sarthak1527@users.noreply.github.com>
2026-02-22 20:09:16 +0545

    added QR code generator script

---
+++ first qrcode generator.py
@@ -0,0 +1,15 @@
+import qrcode
+#Function to generate QR code
+def generate_qr_code(url,file_name):
+#Create QR code instance
+    qr=qrcode.QRCode(version=1,box_size=10,border=5)
+    qr.add_data(url)
+    #Generate QR code
+    qr.make(fit=True)
+    #Create an image from the QR code
+    img=qr.make_image(fill_color="black",back_color="white")
+    img.save(file_name)     
+#enter the url and file name to save the QR code
+url=input("Enter the URL to generate QR code: ")
+file_name=input("Enter the file name to save the QR code (with .png extension): ")
+generate_qr_code(url,file_name) 
